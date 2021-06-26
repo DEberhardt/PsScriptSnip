a8505a07b220ade10ecb33884248f51011b3257b
David Eberhardt <David.Eberhardt@outlook.com>
2021-06-26 19:03:29 +0100

    Adding Match with variable name

---
+++ Snippets/match by content.ps1
@@ -0,0 +1,4 @@
+﻿# content becomes (if matched) a Property of automatic variable $matches
+if ($EffectiveTDP.EffectiveTenantDialPlanName -match '_(?<content>.*)_') {
+  $UserVoiceRouting.EffectiveDialPlan = $matches.content
+}
