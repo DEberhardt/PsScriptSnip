e231679fef91820487e8a388a8649eb2296c486f
David Eberhardt <David.Eberhardt@outlook.com>
2021-11-05 17:20:57 +0000

    Progress bar refit 2

--- Snippets/Better Progress Bars.ps1
+++ Snippets/Better Progress Bars.ps1
@@ -15,7 +15,7 @@ foreach ( $i in 1..10 ) {
 #Initialising Counters
 $script:StepsID0, $script:StepsID1 = Get-WriteBetterProgressSteps -Code $($MyInvocation.MyCommand.Definition) -MaxId 1
 $script:ActivityID0 = $($MyInvocation.MyCommand.Name)
-[int]$script:CountID0 = [int]$script:CountID1 = 1
+[int] $script:CountID0 = [int] $script:CountID1 = 1
 
 
 #PROCESS
