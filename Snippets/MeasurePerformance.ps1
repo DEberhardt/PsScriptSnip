d89fb65f180bcddb47be6dbe49ab8904184a9321
David Eberhardt <David.Eberhardt@outlook.com>
2021-11-13 16:47:17 +0000

    Misc

--- Snippets/MeasurePerformance.ps1
+++ Snippets/MeasurePerformance.ps1
@@ -17,12 +17,12 @@ Write-Host "$Result ms - $Test" -ForegroundColor $Color
 
 
 # Short version (ideal for quick tests)
-$max = 10 # Number of tests
+$max = 1000 # Number of tests
 
 $Test = 'Description'
 Write-Host "$([math]::Round((
   1..$max | Measure-Command -Expression {
     #Test
-
+    
   }
 ).TotalMilliSeconds/$max)) ms - $Test" -ForegroundColor Green
