6236bbca669abd6097e504e1b2b72c14cc23272c
David Eberhardt <david.eberhardt@outlook.com>
2022-02-19 20:45:43 +0000

    Adding One Word Regex

--- Snippets/RegexSnippets.ps1
+++ Snippets/RegexSnippets.ps1
@@ -3,7 +3,7 @@
 # GUID - User accounts (ObjectId), Teams, etc.
 "^[0-9a-f]{8}-([0-9a-f]{4}\-){3}[0-9a-f]{12}$"
 
-# One word
+# One word (no spaces)
 '^[^\W_]+$'
 
 # Abbreviated LineUri (starting with tel:+)
