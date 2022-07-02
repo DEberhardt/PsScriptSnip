5f18ad0166ab0672938f1e46b9de2e34c860d93b
David Eberhardt <david.eberhardt@outlook.com>
2022-07-02 12:15:06 +0100

    Adding selective Module Loader

---
+++ Snippets/Import-Module Selectively.ps1
@@ -0,0 +1,6 @@
+﻿# Using a Module to allow access to private functions
+# This must be the first statement in the script
+using Module AzureAdPreview
+
+# Selectively import specific CmdLets from a different Module - otherwise use Live (AzureAd) Cmdlets
+Import-Module AzureADPreview -Cmdlet Open-AzureADMSPrivilegedRoleAssignmentRequest, Get-AzureADMSPrivilegedRoleAssignment
