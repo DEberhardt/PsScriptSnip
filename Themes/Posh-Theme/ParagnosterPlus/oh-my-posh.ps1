5abbabee662ef1330ffd0fd750c16a7708c22108
David Eberhardt <david.eberhardt@outlook.com>
2022-07-02 14:43:10 +0100

    themes update

---
+++ Themes/Posh-Theme/ParagnosterPlus/oh-my-posh.ps1
@@ -0,0 +1,13 @@
+﻿# installing
+winget install JanDeDobbeleer.OhMyPosh -s winget
+
+# checking source
+(Get-Command oh-my-posh).Source
+
+# updating
+winget upgrade JanDeDobbeleer.OhMyPosh -s winget
+
+
+# load (in profile)
+$UserPsScripts = $PSGetPath.CurrentUserScripts
+oh-my-posh init pwsh --config $UserPsScripts\ParagnosterPlus.omp.json | Invoke-Expression
